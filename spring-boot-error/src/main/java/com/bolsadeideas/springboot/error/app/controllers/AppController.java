7a23ad2b31a7c4245a04bf7fbd721d0aeb9fb337
Antonio Jordan <antoniojordanrodriguez6@gmail.com>
2021-03-23 00:34:12 +0100

    Fin del dia añadidas paginas soporte 404, 500

--- spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -3,10 +3,12 @@ package com.bolsadeideas.springboot.error.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@SuppressWarnings("unused")
 @Controller
 public class AppController {
 	@GetMapping({"/index", "/"})
 	public String index() {
+		Integer valor= 100/0;
 		return "index";
 	}
 }
