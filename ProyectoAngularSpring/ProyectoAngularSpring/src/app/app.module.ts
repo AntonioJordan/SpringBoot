c2b357654106f9b7707dd960f89d102e5d836886
Antonio Jordan <antoniojordanrodriguez6@gmail.com>
2021-04-10 21:07:15 +0200

    Mas componentes

--- ProyectoAngularSpring/ProyectoAngularSpring/src/app/app.module.ts
+++ ProyectoAngularSpring/ProyectoAngularSpring/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule,
